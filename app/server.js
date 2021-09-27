3041d73e83c98e87e43b89390613b875c54cbd33
Kshitiz Tanwar <kshitiz.tanwar@ibm.com>
2021-09-27 15:13:36 -0400

    Added basic homepage for tool

--- app/server.js
+++ app/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 const port = process.env.PORT || 3000;
