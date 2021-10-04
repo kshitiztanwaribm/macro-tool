4e6c4563917aa257cdab71a74ae9bdda1ddfe149
Kshitiz Tanwar <kshitiz.tanwar@ibm.com>
2021-10-04 17:10:25 -0400

    Added upload capability, and made table dynamic

--- app/public/App.controller.js
+++ app/public/App.controller.js
@@ -1,25 +1,54 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast",
-	"sap/ui/model/json/JSONModel"
+	'sap/ui/core/mvc/Controller',
+	'sap/m/MessageToast',
+	'sap/ui/model/json/JSONModel'
 ], function (Controller, MessageToast, JSONModel) {
-	"use strict";
+	'use strict';
 
-	return Controller.extend("DCET.App", {
+	return Controller.extend('DCET.App', {
 		onInit : function () {
+		},
+
+		handleUploadComplete: function(oEvent) {
+			//parse response
+			let res = oEvent.getParameter('response');
+			let htmlDoc = new DOMParser().parseFromString(res, 'text/html');
+			let body = htmlDoc.getElementsByTagName('pre')[0].innerText;
+			body = JSON.parse(body);
+
+			// set table data
+			let cols = body.cols;
+			let rows = body.rows;
+			let errTable = this.byId('errTable');
 			let jModel = new JSONModel();
-			let data = {
-				"Logs": [
-					{"col1": "TEXT", "col2": "TEXT", "col3": 1, "col4": 1, "col5": "TEXT", "col6": 1},
-					{"col1": "TEXT", "col2": "TEXT", "col3": 1, "col4": 1, "col5": "TEXT", "col6": 1},
-					{"col1": "TEXT", "col2": "TEXT", "col3": 1, "col4": 1, "col5": "TEXT", "col6": 1},
-					{"col1": "TEXT", "col2": "TEXT", "col3": 1, "col4": 1, "col5": "TEXT", "col6": 1},
-					{"col1": "TEXT", "col2": "TEXT", "col3": 1, "col4": 1, "col5": "TEXT", "col6": 1},
-					{"col1": "TEXT", "col2": "TEXT", "col3": 1, "col4": 1, "col5": "TEXT", "col6": 1}
-				]
-			}
-			jModel.setData(data);
-			this.getView().setModel(jModel);
+			jModel.setData({
+				rows: rows, columns: cols
+			});
+			errTable.setModel(jModel);
+			errTable.bindColumns('/columns', function(id, context) {
+				let columnName = context.getObject().columnName;
+				return new sap.ui.table.Column({
+					label: columnName,
+					template: columnName,
+				});
+			});
+			errTable.bindRows('/rows');
+			errTable.setBusy(false);
+		},
+
+		handleUploadPress: function(event) {
+			let errTable = this.byId('errTable');
+			errTable.setBusyIndicatorDelay(0);
+			errTable.setBusy(true);
+			
+			let oFileUploader = this.byId('fileUploader');
+			oFileUploader.checkFileReadable().then(function () {
+				oFileUploader.upload();
+			}, function (error) {
+				MessageToast.show('The file cannot be read. It may have changed.');
+			}).then(function () {
+				oFileUploader.clear();
+			});
 		}
 	});
 
--- app/server.js
+++ app/server.js
@@ -1,9 +1,46 @@
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/public'));
+upload = require('express-fileupload');
+app.use(upload());
+
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/upload', function (req, res) {
+  let resObj = {
+    cols: [],
+    rows: []
+  }
+  try {
+    let csvData = req.files.uploader.data.toString('utf8');
+    csvData = csvData.split('\r\n');
+
+    //set cols
+    csvData[1].replace(/"/g, '').split(',').forEach(function (elem) {
+      resObj.cols.push({columnName: elem});
+    });
+
+    //set rows
+    if (csvData.length > 2) {
+      csvData = csvData.slice(2, csvData.length);
+      csvData.forEach(function (elem) {
+        let row = elem.replace(/"/g, '').split(',')
+        let obj = {}
+        for (let i = 0; i < resObj.cols.length; i++) {
+          obj[resObj.cols[i].columnName] = row[i];
+        }
+        resObj.rows.push(obj);
+      });
+    }
+
+    res.status(200);
+  } catch (e) {
+    res.status(500);
+  }
+  return res.send(resObj);
 });
 
 const port = process.env.PORT || 3000;
